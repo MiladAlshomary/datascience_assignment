17bd18d919b2a2b31b305a1fd514068c870e6343
milad <milad@miladalsh.local>
2016-11-21 01:26:40 +0100

    adding enhancment

--- run_analysis.R
+++ run_analysis.R
@@ -1,26 +1,39 @@
-#1.Load test/train data files and merge them into one file
-test_subject <- read.csv('test/subject_test.txt', sep = '', header = FALSE)
-test_features <- read.csv('test/X_test.txt', sep = '', header = FALSE)
-test_label <- read.csv('test/y_test.txt', sep = '', header = FALSE)
+#the code uses dplyr library
+library(dplyr)
 
-train_subject <- read.csv('train/subject_train.txt', sep = '', header = FALSE)
-train_features <- read.csv('train/X_train.txt', sep = '', header = FALSE)
-train_label <- read.csv('train/y_train.txt', sep = '', header = FALSE)
+#1.Load test/train data files
+test_subject <- read.table('test/subject_test.txt', sep = '', header = FALSE)
+test_features <- read.table('test/X_test.txt', sep = '', header = FALSE)
+test_label <- read.table('test/y_test.txt', sep = '', header = FALSE)
 
-test  <- cbind(test_subject, test_features, test_label)
-train <- cbind(train_subject, train_features, train_label)
+train_subject <- read.table('train/subject_train.txt', sep = '', header = FALSE)
+train_features <- read.table('train/X_train.txt', sep = '', header = FALSE)
+train_label <- read.table('train/y_train.txt', sep = '', header = FALSE)
 
-all_data <- rbind(test, train)
 
 #2.Select mean and std measurments for body acc columns
 # the selected columns are: subject id, tBodyAcc-mean()-X, tBodyAcc-mean()-Y,tBodyAcc-mean()-Z,
 # tBodyAcc-std()-X, tBodyAcc-std()-Y, tBodyAcc-std()-Z, y value
-all_data <- all_data[,c(1,2,3,4,5,6,7,563)]
+features_names <- read.table("./features.txt")[,2]
+colnames(train_features) <- features_names
+colnames(test_features) <- features_names
+#select only column with mean/std names
+mean_std_features_names <- grepl("mean|std", features_names)
 
+test_features <- test_features[, mean_std_features_names]
+train_features <- train_features[, mean_std_features_names]
 
-#4.Appropriately labels the data set with descriptive variable names.
-colnames(all_data) <- c('subject', 'x_body_acc_mean', 'y_body_acc_mean', 'z_body_acc_mean', 
-                        'x_body_std_mean', 'y_body_std_mean', 'z_body_std_mean', 'label')
+#merge the files
+test  <- cbind(test_subject,test_label, test_features)
+train <- cbind(train_subject, train_label, train_features)
+
+#merge training and testing tables
+all_data <- rbind(test, train)
+
+#name the first two column
+colnames(all_data)[1] <- 'subject'
+colnames(all_data)[2] <- 'label'
+
 
 #3.Uses descriptive activity names to name the activities in the data set
 all_data$label[all_data$label == 1] <- "WALKING"
@@ -35,8 +48,7 @@ write.table(all_data, './all_data.txt', row.name=FALSE)
 
 #5. create a summary result file by grouping the data by label and subject
 # and calculate average of the measurements.
-summary_result <- summarise(group_by(all_data,subject, label), mean(x_body_acc_mean), mean(y_body_acc_mean), mean(z_body_acc_mean),
-                            mean(x_body_std_mean), mean(y_body_std_mean), mean(z_body_std_mean))
+summary_result <- all_data %>% group_by(subject, label) %>% summarise_each(funs(mean))
 
 # save summary_result datatable into csv file
 write.table(summary_result, './summary_data.txt', row.name=FALSE)
